89a54d5f1b4e2ef242ee2164eb586e54d9b0f3f3
Makarand Kadam <makarand.kadam@anjitait.com>
2026-04-02 13:42:02 +0530

    chore: configure LLDB signal handling and add VS Code launch settings

---
+++ append_lldb.sh
@@ -0,0 +1,3 @@
+echo "process handle -s false -n false -p true EXC_BAD_ACCESS" >> ios/Flutter/ephemeral/flutter_lldbinit
+echo "process handle -s false -n false -p true SIGSEGV" >> ios/Flutter/ephemeral/flutter_lldbinit
+echo "process handle -s false -n false -p true SIGBUS" >> ios/Flutter/ephemeral/flutter_lldbinit
